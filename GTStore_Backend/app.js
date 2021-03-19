926a354eb2dae6610b626e5f8b5789966dc94c97
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2021-03-18 22:36:09 -0600

    finalizando programación backend, queda pendiente ver tema de mostrar todos

--- GTStore_Backend/app.js
+++ GTStore_Backend/app.js
@@ -28,27 +28,61 @@ let producto = {
   product_desc:''
  };
 
-app.post('/crear', function (req, res) {
+app.post('/crear', async function (req, res) {
   producto.product_name=req.body.name
   producto.Cod_prod=req.body.cod
   producto.product_type=req.body.type
   producto.product_price=req.body.price
   producto.product_desc=req.body.desc
-  crearProducto(producto)
-  res.send("enviado")
+  const res_creado= await crearProducto(producto)
+  if(res_creado!='no creado'){
+    res.sendStatus(200)
+  }else{
+    res.sendStatus(404)
+  }
+
+});
+
+app.put('/actualizar', async function (req, res) {
+  producto.product_name=req.body.name
+  producto.Cod_prod=req.body.cod
+  producto.product_type=req.body.type
+  producto.product_price=req.body.price
+  producto.product_desc=req.body.desc
+  const res_act=await actualizarProducto(producto.Cod_prod,producto)
+  if(res_act.matchedCount!='0'){
+    res.sendStatus(201)
+  }else{
+    res.sendStatus(404)
+  }
 });
 
 app.get('/buscar', async function (req, res) {
-    id=req.query.id || 0;
+    id=req.query.id || '';
     if(id!=''){
       const prod= await buscarProducto(id)
-      res.send(prod)
+      if(prod!='sin resultados'){
+        res.send(prod)
+      }else{
+        res.sendStatus(404)
+      }
     }
+});
+app.delete('/eliminar', async function (req, res) {
+  id=req.query.id || '';
+  if(id!=''){
+    const prod= await eliminarProducto(id)
+    if(prod.deletedCount!='0'){
+      res.sendStatus(20o)
+    }else{
+      res.sendStatus(404)
+    }
+  }
 });
 
 app.get('/mostrar', async function (req, res) {
     const prod= await mostrarTodos()
-    res.send(prod)
+    res.send(prod.pretty)
 });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -82,6 +116,38 @@ async function crearProducto(Producto){
       return res
   }
 }
+async function actualizarProducto(Cod,Producto){
+  //string de conexión a base de datos
+  const uri = "mongodb+srv://garydev:adminGTStore@cluster0.xmmh0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  try {
+      await client.connect(); //se apertura la conexión
+      // función para actualizar registro en la base de datos
+      var res= await  updateListingByCod(client,Cod,Producto);
+  } catch (e) {
+    //manejador de errores
+      res= console.error(e);
+  } finally {
+      await client.close(); //Se cierra la conexión
+      return res
+  }
+}
+async function eliminarProducto(Cod){
+  //string de conexión a base de datos
+  const uri = "mongodb+srv://garydev:adminGTStore@cluster0.xmmh0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  try {
+      await client.connect(); //se apertura la conexión
+      // función para actualizar registro en la base de datos
+      var res= await  deleteListingByCod(client,Cod);
+  } catch (e) {
+    //manejador de errores
+      res= console.error(e);
+  } finally {
+      await client.close(); //Se cierra la conexión
+      return res
+  }
+}
 async function buscarProducto(id){
   //string de conexión a base de datos
   const uri = "mongodb+srv://garydev:adminGTStore@cluster0.xmmh0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -106,6 +172,9 @@ async function mostrarTodos(){
       await client.connect(); //se apertura la conexión
       // función para encontrar un registro en la base de datos
      var resultado= await findAll(client);
+     while (resultado.hasNext()) {
+      console.log(resultado.next());
+   }
   } catch (e) {
     //manejador de errores
       resultado = console.error(e);
@@ -125,7 +194,10 @@ async function listDatabases(client){
 async function createListing(client, newListing){
   const result = await client.db("GT_Store_1").collection("Productos").insertOne(newListing);
   console.log(`New listing created with the following id: ${result.insertedId}`);
-  return result.insertedId
+  if(result)
+  {return result.insertedId}
+  else
+  {return 'no creado'}
 }
 
 async function findOneListingById(client, id) {
@@ -136,6 +208,7 @@ async function findOneListingById(client, id) {
       return result
   } else {
       console.log(`No listings found with the id '${id}'`);
+      return 'sin resultados'
   }
 }
 
@@ -143,11 +216,29 @@ async function findAll(client) {
   const result = await client.db("GT_Store_1").collection("Productos").find();
   if (result) {
      // console.log(`Found a listing in the collection with the name '${re}':`);
-      console.log(result);
       return result
   } else {
       console.log('No listings found with the id');
+      return 'sin resultados'
   }
+}
+
+async function updateListingByCod(client, Cod, updatedListing) {
+  const result = await client.db("GT_Store_1").collection("Productos")
+                      .updateOne({ Cod_prod: Cod }, { $set: updatedListing });
+  console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+  console.log(`${result.modifiedCount} document(s) was/were updated.`);
+  return result
+}
+async function deleteListingByCod(client, Cod) {
+  const result = await client.db("GT_Store_1").collection("Productos")
+          .deleteOne({ Cod_prod: Cod });
+  console.log(`${result.deletedCount} document(s) was/were deleted.`);
+  if (result) {
+    return result
+} else {
+    return 'no eliminado'
+}
 }
 
 app.listen(port, () => {
